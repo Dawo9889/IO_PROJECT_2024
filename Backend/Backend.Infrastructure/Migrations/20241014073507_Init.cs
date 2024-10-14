dc3794283966d5be69b8ec9d8772c47e8d169740
Dawid <dawoltis9889@gmail.com>
2024-10-14 09:39:42 +0200

    Dodałem bardzo prosty schemat bazy danych, którzy korzysta z wbudowanego serwera sql w visual studio (Persistance - DbContext). Tworzy prostą encję media, ale to tak tylko poglądaowo

---
+++ Backend/Backend.Infrastructure/Migrations/20241014073507_Init.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medias", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medias");
+        }
+    }
+}
---
+++ Backend/Backend.Infrastructure/Persistance/DbContext.cs
@@ -0,0 +1,27 @@
+﻿using Backend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure.Persistance
+{
+    internal class APIDbContext : DbContext
+    {
+        internal DbSet<Media> Medias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ProjectIO;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Media>();
+        }
+    }
+}
