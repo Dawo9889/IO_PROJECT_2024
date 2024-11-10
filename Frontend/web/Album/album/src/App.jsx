cb653afaf1b092e22fc9fd4eff22db3143b0f577
Hubert <hubert.bojda12@gmail.com>
2024-11-10 18:37:58 +0100

    add page to login, simple html and js, without connect to api ad create routing

--- Frontend/web/Album/album/src/App.jsx
+++ Frontend/web/Album/album/src/App.jsx
@@ -1,11 +1,41 @@
-
-import React, { useState } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import {GiHamburgerMenu} from 'react-icons/gi'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyAlbum from "./MyAlbum/MyAlbum";
+import { useState } from "react";
 import "./App.css";
+import Register from './Components/Navpages/Register';
+import Login from './Components/Navpages/Login';
+import Home from './Components/Navpages/Home';
+import Layout from './Components/Navpages/Layout';
+import Editor from './Components/Navpages/Editor';
+import Admin from './Components/Navpages/Admin';
+import Missing from './Components/Navpages/Missing';
+import Unauthorized from './Components/Navpages/Unauthorized';
+import Lounge from './Components/Navpages/Lounge';
+import LinkPage from './Components/Navpages/LinkPage';
+import RequireAuth from './Components/Navpages/RequireAuth';
 
-function App(){
+function App() {
     return (
-        <MyAlbum />
-   );   
-}
+     <Routes>
+         <Route path="/" element={<Layout />}>
+          {/* public routes */}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="linkpage" element={<LinkPage />} />
+          <Route path="unauthorized" element={<Unauthorized />} />
+  
+          {/* we want to protect these routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="album" element={<MyAlbum />} />
+            <Route path="admin" element={<Admin />} />
+            <Route path="lounge" element={<Lounge />} />
+  
+          {/* catch all */}
+          <Route path="*" element={<Missing />} />
+        </Route>
+     </Routes>
+    );
+  }
 export default App;
--- Frontend/web/Album/album/src/MyAlbum/MyAlbum.jsx
+++ Frontend/web/Album/album/src/MyAlbum/MyAlbum.jsx
@@ -1,5 +1,6 @@
 import HTMLFlipBook from "react-pageflip";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import {GiHamburgerMenu} from 'react-icons/gi'
 import "./MyAlbum.css";
 
 const PageCover = React.forwardRef((props, ref) => {
@@ -23,10 +24,13 @@ const Page = React.forwardRef((props, ref) => {
 });
 
 function MyAlbum(props) {
+    const [checkPrevPage, setCheckPrevPage] = useState(false);
+    const [checkNextPage, setCheckNextPage] = useState(true);
     const [inputText, setInputElement] = useState("");
     const [text, setText] = useState("");
     const [pages, setPages] = useState([{ id: 1, content: "First page" }, { id: 2, content: "Second page" }]);
     const [bookKey, setBookKey] = useState(0); // Key to force re-render
+    const flipBookRef = useRef(); // Reference to HTMLFlipBook
 
     const printText = () => {
         setText(inputText);
@@ -42,50 +46,102 @@ function MyAlbum(props) {
         setBookKey(bookKey + 1); // Change key to force re-render
     };
 
+    const nextPage = () => {
+        if (flipBookRef.current && flipBookRef.current.pageFlip) {
+            const pageFlip = flipBookRef.current.pageFlip();
+        const currentPageIndex = pageFlip.getCurrentPageIndex();
+        const totalPages = pageFlip.getPageCount();
+        console.log(currentPageIndex)
+        if (currentPageIndex + 1 < totalPages) {
+            pageFlip.flipNext();
+            setCheckNextPage(true);
+            setCheckPrevPage(true);
+            if(currentPageIndex == totalPages - 3)
+                {
+                    console.log("elo")
+                    setCheckNextPage(false);
+                }
+        } else {
+            setCheckPrevPage(true);
+            setCheckNextPage(false);
+            console.log(checkNextPage);
+        }
+        }
+    };
+
+    const prevPage = () => {
+        if (flipBookRef.current && flipBookRef.current.pageFlip) {
+            const pageFlip = flipBookRef.current.pageFlip();
+        const currentPageIndex = pageFlip.getCurrentPageIndex();
+        console.log(currentPageIndex)
+        if (currentPageIndex > 0) {
+            pageFlip.flipPrev();
+            setCheckPrevPage(true);
+            setCheckNextPage(true);
+            if(currentPageIndex == 1)
+            {
+                setCheckPrevPage(false);
+            }
+        } else {
+            setCheckNextPage(true);
+            setCheckPrevPage(false);
+            console.log(checkPrevPage);
+        }
+        }
+    };
+
     return (
-        <div style={{ backgroundColor: "LightCyan", minHeight: "100vh", padding: "20px" }}>
-            <HTMLFlipBook
-                key={bookKey} // Key to force re-render on page addition
-                width={550}
-                height={650}
-                minWidth={315}
-                maxWidth={1000}
-                minHeight={420}
-                maxHeight={1350}
-                showCover={true}
-                flippingTime={1000}
-                style={{ margin: "0 auto" }}
-                maxShadowOpacity={0.5}
-                className="album-web"
-            >
-                <PageCover>Wesele //placeholder</PageCover>
-                <PageCover></PageCover>
-                {pages.map((page, index) => (
-                    <Page key={page.id} number={page.id}>
-                        <hr></hr>
-                        <p contentEditable="true">{page.content}</p>
-                    </Page>
-                ))}
-                <PageCover></PageCover>
-                <PageCover>Do zobaczenia na poprawinach</PageCover>
-            </HTMLFlipBook>
-            <br />
-            <div className="formContainer">
-                <input
-                    className="form-control"
-                    value={inputText}
-                    onChange={(e) => setInputElement(e.target.value)}
-                    type="text"
-                    placeholder="Wpisz tekst do wyswietlenia"
-                />
-                <button className="btn" onClick={printText}>
-                    Dodaj naglowek
-                </button>
-                <button className="btn" onClick={addPage}>
-                    Dodaj stron�
-                </button>
+        <>
+            <div style={{ backgroundColor: "white", minHeight: "100vh", padding: "20px", transition: "margin 0.2s", left: "200px" }}>
+                <div className="main-box">
+                    <button className={checkPrevPage ? "btn-nav" : "btn-nav-disabled"} onClick={prevPage}> &larr; </button>    
+                        <HTMLFlipBook
+                            key={bookKey} // Key to force re-render on page addition
+                            width={550}
+                            height={650}
+                            minWidth={315}
+                            maxWidth={1000}
+                            minHeight={420}
+                            maxHeight={1350}
+                            showCover={true}
+                            flippingTime={1000}
+                            style={{ margin: "0 auto" }}
+                            maxShadowOpacity={0.5}
+                            className="album-web"
+                            ref={flipBookRef} // Set reference
+                        >
+                            <PageCover>Wesele //placeholder</PageCover>
+                            <PageCover></PageCover>
+                            {pages.map((page, index) => (
+                                <Page key={page.id} number={page.id}>
+                                    <hr></hr>
+                                    <p contentEditable="true">{page.content}</p>
+                                </Page>
+                            ))}
+                            <PageCover></PageCover>
+                            <PageCover>Do zobaczenia na poprawinach</PageCover>
+                        </HTMLFlipBook>
+                    <button className={checkNextPage ? "btn-nav" : "btn-nav-disabled"} onClick={nextPage}>&rarr;</button>
+                </div>
+                
+                <br />
+                <div className="formContainer">
+                    <input
+                        className="form-control"
+                        value={inputText}
+                        onChange={(e) => setInputElement(e.target.value)}
+                        type="text"
+                        placeholder="Wpisz tekst do wyswietlenia"
+                    />
+                    <button className="btn" onClick={printText}>
+                        Dodaj naglowek
+                    </button>
+                    <button className="btn" onClick={addPage}>
+                        Dodaj stronę
+                    </button>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
