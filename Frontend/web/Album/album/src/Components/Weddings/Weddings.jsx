d2e423c15a4acb0dceae5e1660bf0e7c208a4d1c
Hubert <hubert.bojda12@gmail.com>
2024-11-10 19:06:10 +0100

    create wedding model

---
+++ Frontend/web/Album/album/src/Components/Weddings/Weddings.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Weddings() {
+    const [weddings, setWeddings] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+
+        axios.get('https://localhost:7017/api/wedding/')
+            .then((response) => {
+                setWeddings(response.data);
+            })
+            .catch(() => {
+                setError('Error fetching weddings');
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) return <p>Loading weddings...</p>;
+    if (error) return <p>{error}</p>;
+
+    return (
+        <div>
+            <h1>Wedding List</h1>
+            <ul className='row'>
+                <div className="column">
+                    {weddings.map(wedding => (
+                        <li key={wedding.id}>
+                            <div className='header'>
+                                <h2>{wedding.name}</h2>
+                                <p>Date: {wedding.eventDate}</p>
+                                <p>{wedding.description}</p>
+                            </div>
+                        </li>
+                    ))}
+                </div>
+            </ul>
+        </div>
+    );
+}
