e62e195d5ce5834359538e77819fd5216d2f81ca
jakub stachurski <stachurski.jb@gmail.com>
2024-12-04 19:07:46 +0100

    Only changes in album gallery

--- Frontend/web/Album/album/src/Components/MyAlbum/MyAlbum.jsx
+++ Frontend/web/Album/album/src/Components/MyAlbum/MyAlbum.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/display-name */
 import HTMLFlipBook from "react-pageflip";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import JSZip from "jszip";
 import { saveAs } from "file-saver";
 import jsPDF from "jspdf";
 import { Document, Packer, Paragraph, TextRun, ImageRun } from "docx";
 import html2canvas from "html2canvas";
+import axios from 'axios';
+import Gallery from './Gallery'
 import "./MyAlbum.css";
 
 //okładka przód i tył
@@ -35,7 +37,6 @@ const Page = React.forwardRef((props, ref) => {
     const handleDrop = (e, position) => {
         const src = e.dataTransfer.getData("text");
         props.onImageDrop(src, position, props.number); // Używamy props.number
-        e.target.style.border = "none";
     };
 
     const handleDragOver = (e) => {
@@ -71,23 +72,64 @@ const Page = React.forwardRef((props, ref) => {
 });
 
 //galeria do której należy podpiąć bazę danych ze zdjęciami
-function ImageGallery({ onDragStart }) {
-    const images = ["assets/zdj1.jpg", "assets/zdj2.jpg"];
+function ImageGallery({ onWeddingSelect }) {
+    const [weddings, setWeddings] = useState([]);
+    const [selectedWedding, setSelectedWedding] = useState(null);
+
+    useEffect(() => {
+        const authData = JSON.parse(localStorage.getItem("auth"));
+        const accessToken = authData?.accessToken;
+
+        axios
+            .get(`${import.meta.env.VITE_API_URL}/wedding`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            })
+            .then((response) => {
+                setWeddings(response.data);
+            })
+            .catch((err) => {
+                console.error("Error fetching weddings:", err);
+            });
+    }, []);
+
+    const handleWeddingChange = (event) => {
+        const selectedId = event.target.value;
+        const selectedName = weddings.find((wedding) => wedding.id === selectedId)?.name;
+
+        setSelectedWedding(selectedId);
+
+        if (onWeddingSelect) {
+            onWeddingSelect(selectedId, selectedName);
+        }
+    };
 
     return (
         <div className="image-gallery">
+            <div className="flex items-center justify-between px-4">
+                <select
+                    className="mx-auto w-full max-w-xs p-2 text-white bg-project-dark-bg border border-project-blue rounded-lg focus:outline-none focus:ring-2 focus:ring-project-blue focus:border-transparent"
+                    value={selectedWedding || ""}
+                    onChange={handleWeddingChange}
+                >
+                    <option value="" disabled>
+                        Select Wedding...
+                    </option>
+                    {weddings.map((wedding) => (
+                        <option key={wedding.id} value={wedding.id}>
+                            {wedding.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
             <h3>Galeria zdjęć</h3>
             <div className="gallery-container">
-                {images.map((src, index) => (
-                    <img
-                        key={index}
-                        src={src}
-                        alt={`Gallery ${index + 1}`}
-                        className="gallery-image"
-                        draggable
-                        onDragStart={(e) => onDragStart(e, src)}
-                    />
-                ))}
+                {selectedWedding ? (
+                    <Gallery weddingId={selectedWedding} />
+                ) : (
+                    <p className="text-white"></p>
+                )}
             </div>
         </div>
     );
@@ -482,19 +524,34 @@ function MyAlbum() {
         }
     };
 
+    //zmiana nazw
+    const [selectedWeddingName, setSelectedWeddingName] = useState(null);
+
+    const handleWeddingSelect = (weddingId, weddingName) => {
+        setSelectedWeddingName(weddingName);
+        setPages((prevPages) =>
+            prevPages.map((page) => ({
+                ...page,
+                imageLeft: null,
+                imageLeftBottom: null,
+            }))
+        );
+        console.log("Penis w cieście");
+    };
+
     //główny "html"
     return (
-        <div style={{ backgroundColor: "#CEE5F2", minHeight: "100vh", padding: "20px" }}>
+        <div style={{ backgroundColor: "#20211A", minHeight: "100vh", padding: "20px" }}>
             {!isStaticView ? ( // Jeśli dynamiczny widok
                 <div style={{ display: "flex" }}>
                     <div className="upload-box-container">
-                        <ImageGallery onDragStart={handleDragStart} />
+                        <ImageGallery onDragStart={handleDragStart} onWeddingSelect={handleWeddingSelect} />
                     </div>
                     <div style={{ flex: 1 }}>
                         <HTMLFlipBook
                             key={bookKey}
-                            width={650}
-                            height={750}
+                            width={550}
+                            height={650}
                             minWidth={315}
                             maxWidth={1000}
                             minHeight={420}
@@ -507,7 +564,7 @@ function MyAlbum() {
                             className="album-web"
                             ref={flipBookRef}
                         >
-                            <StartPageCover>Wesele/placeholder</StartPageCover>
+                            <StartPageCover> {selectedWeddingName ? `Album: ${selectedWeddingName}` : "Wybierz wesele"} </StartPageCover>
                             <EndPageCover></EndPageCover>
                             {pages.map((page) => (
                                 <Page
