2688cbc7c5ed2e302765c952278b6722036a6065
Dawo9889 <dawoltis9889@gmail.com>
2024-12-08 17:27:23 +0100

    change error

---
+++ Backend/Backend.API/Override/CustomIdentityErrorDescriber.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Backend.API.Override
+{
+    public class CustomIdentityErrorDescriber : IdentityErrorDescriber
+    {
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return new IdentityError
+            {
+                Code = nameof(DuplicateUserName),
+                Description = $"The email '{userName}' is already in use."
+            };
+        }
+    }
+}
