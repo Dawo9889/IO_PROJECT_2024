904c9203e58aff20e626ebebf7bf27b9a2595bfb
szantonik <szantonik@gmail.com>
2025-01-26 19:53:44 +0100

    App download link updated.

--- Frontend/web/Album/album/src/Components/Other/Mobile.jsx
+++ Frontend/web/Album/album/src/Components/Other/Mobile.jsx
@@ -6,7 +6,7 @@ const Mobile = () => {
 <div className='bg-project-dark-bg rounded-2xl shadow-2xl max-w-full lg:max-w-4/5 p-4 h-full overflow-y-auto'>
         <div className="mb-8 text-center">
             <h1 className="text-3xl font-bold text-white">Download the Mobile App</h1>
-            <a href="https://expo.dev/accounts/skinnyhand/projects/cupid-alpha/builds/78cc1eb1-7a70-41de-a485-0de656e6fe8e" className="mt-4 inline-block bg-project-blue px-6 py-2 rounded-lg shadow-md hover:bg-project-blue-buttons">Download the latest version</a>
+            <a href="https://app.cupid.pics/downloads/cupid.apk" className="mt-4 inline-block bg-project-blue px-6 py-2 rounded-lg shadow-md hover:bg-project-blue-buttons">Download the latest version</a>
         </div>
 
         <div className="mb-8">
